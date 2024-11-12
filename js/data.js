4fa3d2a4be4b59ebfcf0a6622b1851724e898295
Ashley Avena <agshelter7@gmail.com>
2024-11-12 19:06:46 +0000

    add functions that serialize the data model as JSON #1

--- js/data.js
+++ js/data.js
@@ -5,3 +5,15 @@ const data = {
   editing: null,
   nextEntryId: 1,
 };
+function writeData() {
+  const dataJSON = JSON.stringify(data);
+  localStorage.setItem('data-storage', dataJSON);
+}
+function readData() {
+  const dataJSON = localStorage.getItem('data-storage');
+  if (dataJSON) {
+    return JSON.parse(dataJSON);
+  } else {
+    return [];
+  }
+}
--- ts/data.ts
+++ ts/data.ts
@@ -4,3 +4,17 @@ const data = {
   editing: null,
   nextEntryId: 1,
 };
+
+function writeData(): void {
+  const dataJSON: string = JSON.stringify(data);
+  localStorage.setItem('data-storage', dataJSON);
+}
+
+function readData(): any {
+  const dataJSON = localStorage.getItem('data-storage');
+  if (dataJSON) {
+    return JSON.parse(dataJSON);
+  } else {
+    return [];
+  }
+}
